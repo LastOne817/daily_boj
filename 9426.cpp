eac21220d060357127d771ab5730efc8d40b4634
Shin DongJin <dongjin.shin.00@gmail.com>
2018-04-03 15:34:08 +0900

    feat: 9426 AC
    Changed implementation to use multiset.
    Manage two ordered sets to find median (left.rbegin())
    Wasted a lot of time to fix runtime errors regarding empty condition
    check and re-balancing.

--- 9426.cpp
+++ 9426.cpp
@@ -1,30 +1,80 @@
 #include <cstdio>
-#include <cstring>
+#include <set>
+
+using namespace std;
 
 int main(void) {
   int n, k;
-  int count[65536];
-  int t[5000];
   long long sum = 0;
-  scanf("%d %d", &n, &k);
-  for (int i = 0; i < k - 1; i++) {
-    scanf("%d", &t[i % k]);
-  }
-  for (int i = k - 1; i < n; i++) {
-    scanf("%d", &t[i % k]);
+  int pivot;
+  int t[5010];
 
-    memset(count, 0, sizeof(int) * 65536);
-    for (int j = 0; j < k; j++) {
-      count[t[j]]++;
+  multiset<int> left;
+  multiset<int> right;
+  multiset<int>::iterator it;
+
+  scanf("%d %d", &n, &k);
+
+  scanf("%d", &t[0]);
+  left.insert(t[0]);
+  for (int i = 1; i < k; i++) {
+    scanf("%d", &t[i]);
+    if (t[i] < *left.rbegin()) {
+      left.insert(t[i]);
+    } else {
+      right.insert(t[i]);
     }
-    for (int j = 1; j < 65536; j++) {
-      count[j] += count[j - 1];
-      if (count[j - 1] <= (k + 1) / 2 && count[j] > (k + 1) / 2) {
-        sum += j - 1;
-        break;
-      }
+
+    if (left.size() < right.size()) {
+      it = right.begin();
+      left.insert(*it);
+      right.erase(it);
+    } else if (left.size() > right.size() + 1) {
+      it = --left.end();
+      right.insert(*it);
+      left.erase(it);
     }
   }
+  sum += *left.rbegin();
+
+  for (int i = k; i < n; i++) {
+    int t_pop = t[i % k];
+    scanf("%d", &t[i % k]);
+    if (t_pop <= *left.rbegin()) {
+      it = left.find(t_pop);
+      left.erase(it);
+    } else {
+      it = right.find(t_pop);
+      right.erase(it);
+    }
+    if (left.size() < right.size()) {
+      it = right.begin();
+      left.insert(*it);
+      right.erase(it);
+    } else if (left.size() > right.size() + 1) {
+      it = --left.end();
+      right.insert(*it);
+      left.erase(it);
+    }
+
+    if (left.empty() || t[i % k] < *left.rbegin()) {
+      left.insert(t[i % k]);
+    } else {
+      right.insert(t[i % k]);
+    }
+
+    if (left.size() < right.size()) {
+      it = right.begin();
+      left.insert(*it);
+      right.erase(it);
+    } else if (left.size() > right.size() + 1) {
+      it = --left.end();
+      right.insert(*it);
+      left.erase(it);
+    }
+    sum += *left.rbegin();
+  }
+
   printf("%lld\n", sum);
   return 0;
 }
