caefd876697366aa05fcff8db2ee43e12cb314e4
Shin DongJin <dongjin.shin.00@gmail.com>
2018-04-07 13:29:48 +0900

    feat: 11004 AC
    Used max heap to maintain least k numbers.
    If new input is smaller than root of the max heap, remove the root and
    insert the new input. Discard the new input otherwise.

--- 11004.cpp
+++ 11004.cpp
@@ -1,48 +1,27 @@
 #include <cstdio>
-#include <vector>
+#include <queue>
 
 using namespace std;
 
-int qfind_r(vector<int>& arr, int n, int k, int l, int r) {
-  int left = l + 1;
-  int right = r;
-  int pivot = l;
-  int tmp;
-
-  while (left < right) {
-    if (arr[left] <= arr[pivot]) {
-      left++;
-    } else if (arr[right] >= arr[pivot]) {
-      right--;
-    } else {
-      tmp = arr[left];
-      arr[left] = arr[right];
-      arr[right] = tmp;
-    }
-  }
-  if (arr[left] > arr[pivot]) left--;
-  tmp = arr[pivot];
-  arr[pivot] = arr[left];
-  arr[left] = tmp;
-
-  pivot = left;
-
-  if (pivot < k) return qfind_r(arr, n, k, pivot + 1, r);
-  else if (pivot > k) return qfind_r(arr, n, k, l, pivot - 1);
-  else return arr[pivot];
-}
-
 int main(void) {
-  vector<int> arr;
+  priority_queue<int> arr;
   int n, k;
   scanf("%d %d", &n, &k);
-  arr.reserve(n);
 
-  for (int i = 0; i < n; i++) {
-    scanf("%d", &arr[i]);
+  int a;
+  for (int i = 0; i < k; i++) {
+    scanf("%d", &a);
+    arr.push(a);
+  }
+  for (int i = k; i < n; i++) {
+    scanf("%d", &a);
+    if (a < arr.top()) {
+      arr.pop();
+      arr.push(a);
+    }
   }
 
-  printf("%d\n", qfind_r(arr, n, k - 1, 0, n - 1));
+  printf("%d\n", arr.top());
 
   return 0;
 }
