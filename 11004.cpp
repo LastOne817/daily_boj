65aacd0ec819e4f43c2025a4107703ab3c2d5229
Shin DongJin <dongjin.shin.00@gmail.com>
2018-04-07 12:38:15 +0900

    feat: 11004 TLE
    Implemented a quicksort-like recursive search.
    Average O(n), but TLE.

--- 11004.cpp
+++ 11004.cpp
@@ -1,26 +1,48 @@
 #include <cstdio>
-#include <set>
+#include <vector>
 
 using namespace std;
 
-int main(void) {
-  multiset<int> left;
-  int n, k;
-  scanf("%d %d", &n, &k);
+int qfind_r(vector<int>& arr, int n, int k, int l, int r) {
+  int left = l + 1;
+  int right = r;
+  int pivot = l;
+  int tmp;
 
-  int a;
-  for (int i = 0; i < n; i++) {
-    scanf("%d", &a);
-    if (left.size() < k) {
-      left.insert(a);
+  while (left < right) {
+    if (arr[left] <= arr[pivot]) {
+      left++;
+    } else if (arr[right] >= arr[pivot]) {
+      right--;
     } else {
-      if (*left.rbegin() > a) {
-        left.erase(--left.end());
-        left.insert(a);
-      }
+      tmp = arr[left];
+      arr[left] = arr[right];
+      arr[right] = tmp;
     }
   }
-  printf("%d\n", *left.rbegin());
+  if (arr[left] > arr[pivot]) left--;
+  tmp = arr[pivot];
+  arr[pivot] = arr[left];
+  arr[left] = tmp;
+
+  pivot = left;
+
+  if (pivot < k) return qfind_r(arr, n, k, pivot + 1, r);
+  else if (pivot > k) return qfind_r(arr, n, k, l, pivot - 1);
+  else return arr[pivot];
+}
+
+int main(void) {
+  vector<int> arr;
+  int n, k;
+  scanf("%d %d", &n, &k);
+  arr.reserve(n);
+
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &arr[i]);
+  }
+
+  printf("%d\n", qfind_r(arr, n, k - 1, 0, n - 1));
 
   return 0;
 }
