c5bcf681250cbfbd319934087ec42fc89f6d57bd
Shin DongJin <dongjin.shin.00@gmail.com>
2018-04-09 12:38:35 +0900

    feat: 2748 AC
    Simple fibonacci.

---
+++ 2748.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+int main(void) {
+  int n;
+  long long arr[91];
+
+  scanf("%d", &n);
+  arr[0] = 0;
+  arr[1] = 1;
+  for (int i = 2; i <= n; i++) {
+    arr[i] = arr[i - 1] + arr[i - 2];
+  }
+  printf("%lld\n", arr[n]);
+  return 0;
+}
